32393e99a89b20c317106bb585179d81a7bd06ee
刘鹏 <lp_aideals@hotmail.com>
2016-09-06 21:06:51 +0800

    Initial Commit

---
+++ CoreData_test/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CoreData_test
+//
+//  Created by 鹏 刘 on 16/9/6.
+//  Copyright © 2016年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CoreData_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData_test
+//
+//  Created by 鹏 刘 on 16/9/6.
+//  Copyright © 2016年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
